6dc8c263da08d51c0cbeae26e7f580ff628d3059
Alexander <dell-95@mail.ru>
2019-03-30 08:23:27 +0000

    Add simple menu: 1) Add switching between tabs 2) Menu loading in 2D 3) Add switching to VR when loading the main scene

---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine.XR;
+using UnityEngine;
+
+public class Menu : MonoBehaviour
+{
+    public GameObject MainMenu;
+    public GameObject AddImage;
+    public GameObject Gallery;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        XRSettings.enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void PlayClick()
+    {
+        MainMenu.gameObject.SetActive(false);
+        Gallery.gameObject.SetActive(true);
+    }
+
+    public void SettingsClick()
+    {
+        MainMenu.gameObject.SetActive(false);
+        AddImage.gameObject.SetActive(true);
+    }
+
+    public void BackClick()
+    {
+        MainMenu.gameObject.SetActive(true);
+        Gallery.gameObject.SetActive(false);
+        AddImage.gameObject.SetActive(false);
+    }
+
+    public void StartVR()
+    {
+        XRSettings.enabled = true;
+        SceneManager.LoadScene("VR");
+    }
+}
