45774c9a8ecd5ffc3cb5963708e1f59f646be66a
Alexander <dell-95@mail.ru>
2019-04-29 06:42:50 +0000

    Change control in "VR" screen and add "Return to menu" button

--- Assets/Scripts/ControlScript.cs
+++ Assets/Scripts/ControlScript.cs
@@ -1,14 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.SceneManagement;
 using UnityEngine;
 
 public class ControlScript : MonoBehaviour
 {
 
+    public GameObject Menu;
     public int visitorSpeed;
 
     private bool isMoving = false;
     private bool moveForward = true;
+    private bool isPaused = false;
 
     // Start is called before the first frame update
     void Start()
@@ -19,6 +22,7 @@ public class ControlScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        /*
         if (isMoving)
         {
             if (moveForward)
@@ -30,14 +34,40 @@ public class ControlScript : MonoBehaviour
                 transform.position = transform.position - Camera.main.transform.forward * visitorSpeed * Time.deltaTime;
             }
         }
+        */
+        if (Input.GetButton("Fire1") && (!isPaused))
+        {
+            transform.position = transform.position + Camera.main.transform.forward * visitorSpeed * Time.deltaTime;
+        }
     }
 
+    public void PauseClick ()
+    {
+        if (isPaused)
+        {
+            Menu.gameObject.SetActive(false);
+            isPaused = false;
+        }
+        else
+        {
+            isPaused = true;
+            Menu.gameObject.SetActive(true);
+        }
+    }
+
+    public void ReturnToMenuClick ()
+    {
+        Store.vrPicture = null;
+        SceneManager.LoadScene("Menu");
+    }
+
+    /*
     public void MoveForwardUp ()
     {
         isMoving = false;
     }
 
-    public void MoveForwardDown()
+    public void MoveForwardDown ()
     {
         isMoving = true;
         moveForward = true;
@@ -48,9 +78,10 @@ public class ControlScript : MonoBehaviour
         isMoving = false;
     }
 
-    public void MoveBackwardDown()
+    public void MoveBackwardDown ()
     {
         isMoving = true;
         moveForward = false;
     }
+    */
 }
